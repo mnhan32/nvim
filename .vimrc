2d0fd07ad25b696728f1c36f4a73b45be8c31d0d
mnhan32 <mnhan32@gmail.com>
2024-01-11 23:27:27 +0800

    adjust keymap, keep a copy of .vimrc here

---
+++ .vimrc
@@ -0,0 +1,39 @@
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+set smarttab
+set rnu
+set splitbelow
+set splitright
+set mouse=i
+syntax on
+colorscheme slate
+
+"keymap
+let mapleader = " "
+nnoremap <leader>e :Ex<Cr>
+
+"system clipboard
+"use vim comes with gvim on archlinux
+"beacuse vim installed with pacman disable system clipboard
+set clipboard=unnamedplus
+"set cut copy and paste
+"system
+vnoremap <leader>y "+y
+vnoremap <leader>d "+d
+nnoremap <leader>p "+p
+"buffer
+vnoremap <leader>Y "*y
+vnoremap <leader>D "*d
+nnoremap <leader>P "*p
+
+
+"move lines use ctrl-shift j/k
+nnoremap <S-C-j> :m .+1<CR>==
+nnoremap <S-C-k> :m .-2<CR>==
+inoremap <S-C-j> <Esc>:m .+1<CR>==gi
+inoremap <S-C-k> <Esc>:m .-2<CR>==gi
+vnoremap <S-C-j> :m '>+1<Cr>gv=gv
+vnoremap <S-C-k> :m '<-2<Cr>gv=gv
+
--- init.lua
+++ init.lua
@@ -9,12 +9,29 @@ vim.o.splitbelow = true
 vim.o.splitright =  true
 vim.o.mouse = "i"
 
+--use system clipboard,
+--on archlinux, install xclip for X11 system clipboard
+--because nvim on archlinux pacman compile nvim without clipboard
+vim.opt.clipboard:append { "unnamed", "unnamedplus" }
+--system clipboard, do not how to keep "*p after paste 
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>d", '"+d')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>Y", '"*y')
+vim.keymap.set("v", "<leader>D", '"*d')
+vim.keymap.set("n", "<leader>P", '"*p')
+
 -- setup leader key
 vim.g.mapleader = " "
 -- basic key remap
+--move lines with ctrl-shift j/k in various modes
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-S-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<C-S-k>", ":m .-2<CR>==")
+vim.keymap.set("i", "<C-S-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("i", "<C-S-k>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("v", "<C-S-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-S-k>", ":m '<-2<CR>gv=gv")
 
 --enable exrc
 vim.o.exrc = true
