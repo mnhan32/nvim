0cf9e9c562150cdf2805095e1c323203f8829fe4
mnhan32 <mnhan32@gmail.com>
2024-01-16 08:26:11 +0800

    set up lsp and formatter, need to work on cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
-return{
+return {
   {
     'williamboman/mason.nvim',
     config = function()
@@ -33,14 +33,32 @@ return{
     'rafamadriz/friendly-snippets',
   },
   {
+    'lukas-reineke/lsp-format.nvim',
+    config = function()
+      require("lsp-format").setup {}
+    end
+  },
+  {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     config = function()
-      local lsp_zero=require('lsp-zero')
+      local lsp_zero = require('lsp-zero')
+      local lsp_format = require('lsp-format')
       lsp_zero.extend_lspconfig()
       lsp_zero.on_attach(function(client, bufnr)
         -- keymap on attach
-        lsp_zero.default_keymaps({buffer = bufnr})
+        lsp_zero.default_keymaps({ buffer = bufnr })
+        --
+        -- this is to make sure client has format capbilities
+        -- lsp-format provide async way
+        if client.supports_method('textdocument/formatting') then
+          lsp_format.on_attach(client)
+        end
+        --
+        local opts = { buffer = bufnr }
+        vim.keymap.set({ 'n', 'x' }, 'gq', function()
+          vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+        end, opts)
       end)
       -- setup lua
       require("lspconfig").lua_ls.setup({})
@@ -61,24 +79,23 @@ return{
       --
       -- ******
       --cmp autocomplete/suggestion with luasnip ui
-      --lsp_zero will set up autocomplete
-      --
+      --lsp_zero will set up autocomplete, the following are ref
       --local cmp = require("cmp")
       --local cmp_select = {behavior = cmp.SelectBehavior.Select}
       --cmp.setup({
       --  source = {
-      --  {name = 'path'},
-      --  {name = 'nvim_lsp'},
-      --  {name = 'nvim_lua'},
-      --  {name = 'luasnip', keyword_length = 3},
-      --  {name = 'buffer', keyword_length = 3},
+      --    {name = 'path'},
+      --    {name = 'nvim_lsp'},
+      --    {name = 'nvim_lua'},
+      --    {name = 'luasnip', keyword_length = 3},
+      --    {name = 'buffer', keyword_length = 3},
       --  },
       --  formatting = lsp_zero.cmp_format(),
       --  mapping = cmp.mapping.preset.insert({
       --    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
       --    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
       --    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-      --    ['<C-Space>'] = cmp.mapping.complete(),
+      --    ['<C-S-Space>'] = cmp.mapping.complete(),
       --  }),
       --})
       --
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,21 @@
-return{
+return {
   {
-     "nvim-treesitter/nvim-treesitter",
-     build = ":TSUpdate",
-     config = function()
-       require("nvim-treesitter.configs").setup({
-         ensure_installed = { 
-            "c", "lua", "python", "markdown", 
-            "vim", "vimdoc", "query", "elixir", 
-            "heex", "javascript", "html" },
-         sync_install = false,
-         highlight = { enable = true },
-         indent = { enable = true },  
-       })
-     end
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      -- use curl, for some reason git is not stable on my laptop
+      require("nvim-treesitter.install").prefer_git = false
+      -- on window 64, I use zig to compile parser
+      require("nvim-treesitter.install").compilier = { "clang", "gcc", "zig", "msvc" }
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "c", "lua", "python", "markdown",
+          "vim", "vimdoc", "query", "elixir",
+          "heex", "javascript", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
   },
 }
