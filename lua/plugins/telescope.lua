189e490e7f07625f4c80d582457794525e16008b
mnhan32 <mnhan32@gmail.com>
2024-01-18 21:00:09 +0800

    add telescope-ui-select for session manager

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,31 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',
-  -- or                              , branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Grep" })
-    vim.keymap.set('n', '<leader>fw', builtin.current_buffer_fuzzy_find, { desc = "Find in current buffer" })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help Tags" })
-  end
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Grep" })
+      vim.keymap.set('n', '<leader>fw', builtin.current_buffer_fuzzy_find, { desc = "Find in current buffer" })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help Tags" })
+    end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      local telescope = require("telescope")
+      local telescope_themes = require("telescope.themes")
+      telescope.setup({
+        extensions = {
+          ["ui-select"] = {
+            telescope_themes.get_dropdown {}
+          }
+        }
+      })
+      telescope.load_extension("ui-select")
+    end,
+  }
 }
