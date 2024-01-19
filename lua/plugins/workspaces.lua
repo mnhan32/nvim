14a09b17b248d30aac70af76f6c594d7fde31406
han_ho 何孟翰 <han_ho@brogent.com>
2024-01-19 18:09:22 +0800

    group some keymap, need to rename whichkey group prefix name

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -12,13 +12,15 @@ function WorksapceAddUI()
   end
   )
 end
-vim.keymap.set('n', '<leader>wa', WorksapceAddUI)
+
+vim.keymap.set('n', '<leader>w', '<Nop>', { desc = "+Workspaces" })
+vim.keymap.set('n', '<leader>wa', WorksapceAddUI, { desc = "Add Workspace" })
 
 return {
   "natecraddock/workspaces.nvim",
   config = function()
     require("workspaces").setup({
-      vim.keymap.set('n', '<leader>fp', '<CMD>Telescope workspaces<CR>', { desc = "Find Workspaces" }),
+      vim.keymap.set('n', '<leader>ws', '<CMD>Telescope workspaces<CR>', { desc = "Find Workspaces" }),
     })
     local telescope = require("telescope")
     telescope.load_extension("workspaces")
