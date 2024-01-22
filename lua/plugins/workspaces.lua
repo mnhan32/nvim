a4711ad0d2d88d41b0493ba03923410c328658c7
han_ho 何孟翰 <han_ho@brogent.com>
2024-01-22 14:14:45 +0800

    switch back to session-manager

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -16,32 +16,32 @@ end
 vim.keymap.set('n', '<leader>ww', WorksapceAddUI, { desc = "Add Workspace" })
 
 return {
-  {
-    "natecraddock/sessions.nvim",
-    config = function()
-      local sessions = require("sessions")
-      sessions.setup({
-        events = { "VimLeavePre" },
-        session_filepath = vim.fn.stdpath("data") .. "/sessions",
-        absolute = true,
-      })
-      vim.keymap.set('n', '<leader>ws', '<CMD>SessionsSave<CR>', { desc = 'Save Session' })
-      vim.keymap.set('n', '<leader>wr', '<CMD>SeessionsLoad<CR>', { desc = 'Load Session' })
-    end
-  },
+  --{
+  --  "natecraddock/sessions.nvim",
+  --  config = function()
+  --    local sessions = require("sessions")
+  --    sessions.setup({
+  --      events = { "VimLeavePre" },
+  --      session_filepath = vim.fn.stdpath("data") .. "/sessions",
+  --      absolute = true,
+  --    })
+  --    vim.keymap.set('n', '<leader>ws', '<CMD>SessionsSave<CR>', { desc = 'Save Session' })
+  --    vim.keymap.set('n', '<leader>wr', '<CMD>SeessionsLoad<CR>', { desc = 'Load Session' })
+  --  end
+  --},
   {
     "natecraddock/workspaces.nvim",
     config = function()
       local workspaces = require("workspaces")
       workspaces.setup({
         vim.keymap.set('n', '<leader>wf', '<CMD>Telescope workspaces<CR>', { desc = "Find Workspaces" }),
-        auto_open = true,
-        hooks = {
-          open = function()
-            local sessions = require("sessions")
-            sessions.load(nil, { silent = true })
-          end
-        },
+        --auto_open = true,
+        --hooks = {
+        --  open = function()
+        --    local sessions = require("sessions")
+        --    sessions.load(nil, { silent = true })
+        --  end
+        --},
       })
       local telescope = require("telescope")
       telescope.load_extension("workspaces")
