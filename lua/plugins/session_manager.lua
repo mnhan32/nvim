c91d5ca88ac4fcdf0040fe82c6eef2f6e534d70b
mnhan32 <mnhan32@gmail.com>
2024-01-23 00:23:12 +0800

    change lualine_c to workspace path

--- lua/plugins/session_manager.lua
+++ lua/plugins/session_manager.lua
@@ -3,7 +3,15 @@ return {
     "Shatur/neovim-session-manager",
     config = function()
       local session = require("session_manager")
+      local Path = require("plenary.path")
+      local config = require("session_manager.config")
       session.setup({
+        autoload_mode = config.AutoloadMode.CurrentDir,
+        autosave_last_session = false,
+        autosave_ignore_filetypes = {
+          'gitcommit',
+          'gitrebase'
+        },
         vim.keymap.set("n", "<leader>sl", "<CMD>SessionManager load_session<CR>", { desc = "Load Sessions" }),
         vim.keymap.set("n", "<leader>sd", "<CMD>SessionManager delete_session<CR>", { desc = "Delete Sessions" }),
         vim.keymap.set("n", "<leader>ss", "<CMD>SessionManager save_current_session<CR>", { desc = "Save Session" }),
--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -16,32 +16,29 @@ end
 vim.keymap.set('n', '<leader>ww', WorksapceAddUI, { desc = "Add Workspace" })
 
 return {
-  --{
-  --  "natecraddock/sessions.nvim",
-  --  config = function()
-  --    local sessions = require("sessions")
-  --    sessions.setup({
-  --      events = { "VimLeavePre" },
-  --      session_filepath = vim.fn.stdpath("data") .. "/sessions",
-  --      absolute = true,
-  --    })
-  --    vim.keymap.set('n', '<leader>ws', '<CMD>SessionsSave<CR>', { desc = 'Save Session' })
-  --    vim.keymap.set('n', '<leader>wr', '<CMD>SeessionsLoad<CR>', { desc = 'Load Session' })
-  --  end
-  --},
   {
     "natecraddock/workspaces.nvim",
     config = function()
       local workspaces = require("workspaces")
       workspaces.setup({
         vim.keymap.set('n', '<leader>wf', '<CMD>Telescope workspaces<CR>', { desc = "Find Workspaces" }),
-        --auto_open = true,
-        --hooks = {
-        --  open = function()
-        --    local sessions = require("sessions")
-        --    sessions.load(nil, { silent = true })
-        --  end
-        --},
+        auto_open = true,
+        hooks = {
+          open = function()
+            local sessions = require("session_manager")
+            local lualine = require("lualine")
+            local function custom_lualine_c()
+              return "Current Workspaces : " .. workspaces.path()
+            end
+            lualine.setup({
+              sections = {
+                lualine_c = { custom_lualine_c }
+              }
+            })
+            sessions.load_current_dir_session(true)
+            --sessions.load(nil, { silent = true })
+          end
+        },
       })
       local telescope = require("telescope")
       telescope.load_extension("workspaces")
