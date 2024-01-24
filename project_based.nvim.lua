51a861a96def0e4251d53ae5ec46dc3b9506a419
mnhan32 <mnhan32@gmail.com>
2024-01-25 00:02:33 +0800

    try after and ftplugin folder for filetype, but these two are in .gitignore, it's for user to overwrite setting.

--- project_based.nvim.lua
+++ project_based.nvim.lua
@@ -1,5 +1,7 @@
 -- nvim will load .exrc .nvimrc .nvim.lua located in current directory
 -- copy this file to your project root directory for project based nvimsettings
-
+-- or simply use FileType( :help ftplugin  )
+-- copy and paste the following to a python.lua under ftplugin folder
+-- it will set the variable only if you are working with python file type
 vim.g.python_host_prog = "/home/han/.pyenv/versions/3.9.1/envs/pxr-usd/bin/python3"
 vim.g.python3_host_prog = "/home/han/.pyenv/versions/3.9.1/envs/pxr-usd/bin/python3"
