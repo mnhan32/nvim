71275f1f393982b23cd58e064464aabecedd8d10
han_ho 何孟翰 <han_ho@brogent.com>
2024-01-25 14:21:27 +0800

    add example for editting system path in ftplugin or .nvim.lua

--- project_based.nvim.lua
+++ project_based.nvim.lua
@@ -1,7 +1,15 @@
+--
+-- This can be used by putting .nvim.lua in yout workspace root, or python.lua in ftplugin path
+--
 -- nvim will load .exrc .nvimrc .nvim.lua located in current directory
 -- copy this file to your project root directory for project based nvimsettings
 -- or simply use FileType( :help ftplugin  )
 -- copy and paste the following to a python.lua under ftplugin folder
 -- it will set the variable only if you are working with python file type
-vim.g.python_host_prog = "/home/han/.pyenv/versions/3.9.1/envs/pxr-usd/bin/python3"
-vim.g.python3_host_prog = "/home/han/.pyenv/versions/3.9.1/envs/pxr-usd/bin/python3"
+
+local user_profile = os.getenv("UserProfile")
+local local_site_packages = user_profile .. "\\AppData\\Roaming\\Python\\Python39\\site-packages"
+local local_script = user_profile .. "\\AppData\\Roaming\\Python\\Python39\\Scripts"
+vim.env.Path = vim.env.Path .. ";" .. local_site_packages .. ";" .. local_script
+vim.g.python_host_prog = "U:\\Python\\Python309\\python.exe"
+vim.g.python3_host_prog = "U:\\Python\\Python309\\python.exe"
