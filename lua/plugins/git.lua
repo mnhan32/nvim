29f89f887aad36d4a3378edf06d52d595302c749
mnhan32 <mnhan32@gmail.com>
2024-01-31 22:20:33 +0800

    wrong gitsigns urlm, but removed it anyway.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,10 +1,3 @@
 return {
-  {
-    "tpope/gitsigns.nvim",
-    config = function()
-      local gitsigns = require("gitsigns")
-      gitsigns.setup()
-    end
-  },
   { "tpope/vim-fugitive" },
 }
