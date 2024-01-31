16c2210197a10938682d3dd390ed5ff430b2d1c7
mnhan32 <mnhan32@gmail.com>
2024-01-31 23:12:05 +0800

    remove gitsign after test

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,3 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      local gitsigns = require("gitsigns")
-      gitsigns.setup()
-    end
-
-  },
   { "tpope/vim-fugitive" },
 }
