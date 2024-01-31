70dffa576681504ca735b723644a0af8416e6f45
mnhan32 <mnhan32@gmail.com>
2024-01-31 22:27:40 +0800

    try gitsign again

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,11 @@
 return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gitsigns = require("gitsigns")
+      gitsigns.setup()
+    end
+
+  },
   { "tpope/vim-fugitive" },
 }
